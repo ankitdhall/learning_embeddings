655116785a8be27e2b92ce27b59a01fa8b5c1c9b
ankitdhall <ankitdhall2005@yahoo.co.in>
2019-03-11 16:19:54 +0100

    add additional md func; hrule, text, image

--- network/summarize.py
+++ network/summarize.py
@@ -7,7 +7,7 @@ class Summarize:
         self.markdown_path = os.path.join(self.log_dir, 'summary.md')
         self.markdown = open(self.markdown_path, 'w')
 
-    def make_title(self, heading, heading_level=1):
+    def make_heading(self, heading, heading_level=1):
         self.markdown.write('{} {}  \n'.format('#'*heading_level, heading))
 
     def make_table(self, data, x_labels=None, y_labels=None):
@@ -34,14 +34,39 @@ class Summarize:
             table_str += text_row + '  \n'
         self.markdown.write(table_str)
 
+    def make_text(self, text):
+        self.markdown.write(text + '  \n')
+
+    def make_image(self, location, alt_text):
+        self.markdown.write('![{1}]({0} "{1}")  \n'.format(location, alt_text))
+
+    def make_hrule(self):
+        self.markdown.write('---  \n')
+
 
 if __name__ == '__main__':
-    s = Summarize('')
-    s.make_title('Test')
+    s = Summarize('../exp/alexnet_ft')
+    s.make_heading('Test')
     x_labels = ['a', 'b', 'c']
     y_labels = ['x', 'y']
     data = [[1, 2, 3], [4, 5, 6]]
     s.make_table(data, x_labels, y_labels)
+    s.make_hrule()
+    s.make_image('../../database/hymenoptera_data/train/ants/0013035.jpg', 'ants_0013035')
+    s.make_text('Ground-truth label: ant')
+    s.make_text('Predicted label: bee')
+    s.make_hrule()
+    s.make_image('../../database/hymenoptera_data/train/ants/154124431_65460430f2.jpg', 'ants_154124431_65460430f2')
+    s.make_text('Ground-truth label: ant')
+    s.make_text('Predicted label: bee')
+    s.make_hrule()
+    s.make_image('../../database/hymenoptera_data/train/ants/0013035.jpg', 'ants_0013035')
+    s.make_text('Ground-truth label: ant')
+    s.make_text('Predicted label: bee')
+    s.make_hrule()
+    s.make_image('../../database/hymenoptera_data/train/ants/154124431_65460430f2.jpg', 'ants_154124431_65460430f2')
+    s.make_text('Ground-truth label: ant')
+    s.make_text('Predicted label: bee')
 
 
 

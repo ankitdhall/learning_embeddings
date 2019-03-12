edb9451c92752d7a6e079f0071c4cdd329612d76
ankitdhall <ankitdhall2005@yahoo.co.in>
2019-03-12 11:43:32 +0100

    correct paths; allow bullets in text

--- network/summarize.py
+++ network/summarize.py
@@ -5,10 +5,10 @@ class Summarize:
     def __init__(self, log_dir):
         self.log_dir = log_dir
         self.markdown_path = os.path.join(self.log_dir, 'summary.md')
-        self.markdown = open(self.markdown_path, 'w')
+        self.markdown = open(self.markdown_path, 'wt')
 
     def make_heading(self, heading, heading_level=1):
-        self.markdown.write('{} {}  \n'.format('#'*heading_level, heading))
+        self.markdown.write('{} {}\n\n'.format('#'*heading_level, heading))
 
     def make_table(self, data, x_labels=None, y_labels=None):
         table = []
@@ -34,14 +34,14 @@ class Summarize:
             table_str += text_row + '  \n'
         self.markdown.write(table_str)
 
-    def make_text(self, text):
-        self.markdown.write(text + '  \n')
+    def make_text(self, text, bullet=True):
+        self.markdown.write(('- ' if bullet else '') + text + '  \n')
 
     def make_image(self, location, alt_text):
-        self.markdown.write('![{1}]({0} "{1}")  \n'.format(location, alt_text))
+        self.markdown.write('![{}]({})\n\n'.format(alt_text, os.path.relpath(location, self.log_dir)))
 
     def make_hrule(self):
-        self.markdown.write('---  \n')
+        self.markdown.write('---\n\n')
 
 
 if __name__ == '__main__':
@@ -52,19 +52,19 @@ if __name__ == '__main__':
     data = [[1, 2, 3], [4, 5, 6]]
     s.make_table(data, x_labels, y_labels)
     s.make_hrule()
-    s.make_image('../../database/hymenoptera_data/train/ants/0013035.jpg', 'ants_0013035')
+    s.make_image('/home/ankit/learning_embeddings/database/hymenoptera_data/train/ants/0013035.jpg', 'ants_0013035')
     s.make_text('Ground-truth label: ant')
     s.make_text('Predicted label: bee')
     s.make_hrule()
-    s.make_image('../../database/hymenoptera_data/train/ants/154124431_65460430f2.jpg', 'ants_154124431_65460430f2')
+    s.make_image('/home/ankit/learning_embeddings/database/hymenoptera_data/train/ants/154124431_65460430f2.jpg', 'ants_154124431_65460430f2')
     s.make_text('Ground-truth label: ant')
     s.make_text('Predicted label: bee')
     s.make_hrule()
-    s.make_image('../../database/hymenoptera_data/train/ants/0013035.jpg', 'ants_0013035')
+    s.make_image('/home/ankit/learning_embeddings/database/hymenoptera_data/train/ants/0013035.jpg', 'ants_0013035')
     s.make_text('Ground-truth label: ant')
     s.make_text('Predicted label: bee')
     s.make_hrule()
-    s.make_image('../../database/hymenoptera_data/train/ants/154124431_65460430f2.jpg', 'ants_154124431_65460430f2')
+    s.make_image('/home/ankit/learning_embeddings/database/hymenoptera_data/train/ants/154124431_65460430f2.jpg', 'ants_154124431_65460430f2')
     s.make_text('Ground-truth label: ant')
     s.make_text('Predicted label: bee')
 

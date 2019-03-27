7f8529bc88dcc3a3caa50c998969d7d44d05c9f6
ankitdhall <ankitdhall2005@yahoo.co.in>
2019-03-27 17:22:28 +0100

    functionality to purge json

--- data/purge_database.py
+++ data/purge_database.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import cv2
 
 
-def purge_data(data, images_dir, clean_dir):
+def purge_image_data(data, images_dir, clean_dir):
     for data_id in tqdm(data):
         sample = data[data_id]
         if sample['family'] not in ['Papilionidae', 'Pieridae', 'Nymphalidae', 'Lycaenidae', 'Hesperiidae', 'Riodinidae']:
@@ -26,11 +26,25 @@ def purge_data(data, images_dir, clean_dir):
         cv2.imwrite(os.path.join(path_to_clean_dir, sample['image_name']), img)
 
 
+def purge_json_data(data, json_path):
+    purged_data = {}
+
+    for data_id in tqdm(data):
+        sample = data[data_id]
+        if sample['family'] not in ['Papilionidae', 'Pieridae', 'Nymphalidae', 'Lycaenidae', 'Hesperiidae', 'Riodinidae']:
+            continue
+        purged_data[data_id] = sample
+
+    with open(json_path, 'w') as outfile:
+        json.dump(purged_data, outfile, indent=4)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--mini", help='Use the mini database for testing/debugging.', action='store_true')
     parser.add_argument("--images_dir", help='Parent directory with images.', type=str, required=True)
     parser.add_argument("--clean_dir", help='Clean directory with images.', type=str, required=True)
+    parser.add_argument("--json_path", help='Path to json with relevant data.', type=str, required=True)
     args = parser.parse_args()
 
     infile = 'database'
@@ -44,4 +58,5 @@ if __name__ == '__main__':
         print("File does not exist!")
         exit()
 
-    purge_data(data, args.images_dir, args.clean_dir)
+    # purge_image_data(data, args.images_dir, args.clean_dir)
+    purge_json_data(data, args.json_path)

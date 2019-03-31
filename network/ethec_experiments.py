baf6a92891522d9e49ee198a715fc996f186faf3
ankitdhall <ankitdhall2005@yahoo.co.in>
2019-03-31 13:24:16 +0200

    add cmd args for image dir

---
+++ network/ethec_experiments.py
@@ -0,0 +1,162 @@
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, models, transforms
+
+import os
+from experiment import Experiment
+from evaluation import MLEvaluation, Evaluation, MLEvaluationSingleThresh
+from finetuner import CIFAR10
+
+from data.db import ETHECLabelMap, Rescale, ToTensor, Normalize, ETHECDB
+
+from PIL import Image
+import numpy as np
+
+import copy
+import argparse
+import json
+
+
+class ETHECExperiment(CIFAR10):
+    def __init__(self, data_loaders, labelmap, criterion, lr,
+                 batch_size,
+                 evaluator,
+                 experiment_name,
+                 experiment_dir='../exp/',
+                 n_epochs=10,
+                 eval_interval=2,
+                 feature_extracting=True,
+                 use_pretrained=True,
+                 load_wt=False,
+                 model_name=None):
+        CIFAR10.__init__(self, data_loaders, labelmap, criterion, lr, batch_size, evaluator, experiment_name,
+                         experiment_dir, n_epochs, eval_interval, feature_extracting, use_pretrained,
+                         load_wt, model_name)
+
+
+def ETHEC_train_model(arguments):
+    if not os.path.exists(os.path.join(arguments.experiment_dir, arguments.experiment_name)):
+        os.makedirs(os.path.join(arguments.experiment_dir, arguments.experiment_name))
+    with open(os.path.join(arguments.experiment_dir, arguments.experiment_name, 'config_params.txt'), 'w') as file:
+        file.write(json.dumps(vars(args), indent=4))
+
+    print('Config parameters for this run are:\n{}'.format(json.dumps(vars(args), indent=4)))
+
+    input_size = 224
+    labelmap = ETHECLabelMap()
+    data_transforms = transforms.Compose([Rescale((input_size, input_size)),
+                                          ToTensor(),
+                                          Normalize(mean=(143.2341, 162.8151, 177.2185),
+                                                    std=(66.7762, 59.2524, 51.5077))])
+
+    train_set = ETHECDB(path_to_json='../database/ETHEC/train.json',
+                        path_to_images='/media/ankit/DataPartition/IMAGO_build/',
+                        labelmap=labelmap, transform=data_transforms)
+    val_set = ETHECDB(path_to_json='../database/ETHEC/val.json',
+                      path_to_images='/media/ankit/DataPartition/IMAGO_build/',
+                      labelmap=labelmap, transform=data_transforms)
+    test_set = ETHECDB(path_to_json='../database/ETHEC/test.json',
+                       path_to_images='/media/ankit/DataPartition/IMAGO_build/',
+                       labelmap=labelmap, transform=data_transforms)
+    print('Dataset has following splits: train: {}, val: {}, test: {}'.format(len(train_set), len(val_set),
+                                                                              len(test_set)))
+
+    batch_size = arguments.batch_size
+    n_workers = arguments.n_workers
+
+    if arguments.debug:
+        print("== Running in DEBUG mode!")
+        train_set = ETHECDB(path_to_json='../database/ETHEC/train.json',
+                            path_to_images='/media/ankit/DataPartition/IMAGO_build/',
+                            labelmap=labelmap, transform=data_transforms)
+        val_set = ETHECDB(path_to_json='../database/ETHEC/val.json',
+                          path_to_images='/media/ankit/DataPartition/IMAGO_build/',
+                          labelmap=labelmap, transform=data_transforms)
+        test_set = ETHECDB(path_to_json='../database/ETHEC/test.json',
+                           path_to_images='/media/ankit/DataPartition/IMAGO_build/',
+                           labelmap=labelmap, transform=data_transforms)
+        trainloader = torch.utils.data.DataLoader(torch.utils.data.Subset(train_set, list(range(100))),
+                                                  batch_size=batch_size,
+                                                  shuffle=True, num_workers=n_workers)
+
+        valloader = torch.utils.data.DataLoader(torch.utils.data.Subset(train_set, list(range(100, 200))),
+                                                batch_size=batch_size,
+                                                shuffle=True, num_workers=n_workers)
+
+        testloader = torch.utils.data.DataLoader(torch.utils.data.Subset(train_set, list(range(200, 300))),
+                                                 batch_size=batch_size,
+                                                 shuffle=False, num_workers=n_workers)
+
+        data_loaders = {'train': trainloader, 'val': valloader, 'test': testloader}
+
+    else:
+        train_set = ETHECDB(path_to_json='../database/ETHEC/train.json',
+                            path_to_images=args.image_dir,
+                            labelmap=labelmap, transform=data_transforms)
+        val_set = ETHECDB(path_to_json='../database/ETHEC/val.json',
+                          path_to_images=args.image_dir,
+                          labelmap=labelmap, transform=data_transforms)
+        test_set = ETHECDB(path_to_json='../database/ETHEC/test.json',
+                           path_to_images=args.image_dir,
+                           labelmap=labelmap, transform=data_transforms)
+
+        trainloader = torch.utils.data.DataLoader(train_set,
+                                                  batch_size=batch_size,
+                                                  shuffle=True, num_workers=n_workers)
+        valloader = torch.utils.data.DataLoader(val_set,
+                                                batch_size=batch_size,
+                                                shuffle=True, num_workers=n_workers)
+        testloader = torch.utils.data.DataLoader(test_set,
+                                                 batch_size=batch_size,
+                                                 shuffle=False, num_workers=n_workers)
+
+        data_loaders = {'train': trainloader, 'val': valloader, 'test': testloader}
+
+    eval_type = MLEvaluation(os.path.join(arguments.experiment_dir, arguments.experiment_name), labelmap)
+    if arguments.evaluator == 'MLST':
+        eval_type = MLEvaluationSingleThresh(os.path.join(arguments.experiment_dir, arguments.experiment_name),
+                                             labelmap)
+
+    ETHEC_trainer = ETHECExperiment(data_loaders=data_loaders, labelmap=labelmap,
+                                    criterion=nn.MultiLabelSoftMarginLoss(),
+                                    lr=arguments.lr,
+                                    batch_size=batch_size, evaluator=eval_type,
+                                    experiment_name=arguments.experiment_name,  # 'cifar_test_ft_multi',
+                                    experiment_dir=arguments.experiment_dir,
+                                    eval_interval=arguments.eval_interval,
+                                    n_epochs=arguments.n_epochs,
+                                    feature_extracting=arguments.freeze_weights,
+                                    use_pretrained=True,
+                                    load_wt=False,
+                                    model_name=arguments.model)
+    ETHEC_trainer.prepare_model()
+    if arguments.set_mode == 'train':
+        ETHEC_trainer.train()
+    elif arguments.set_mode == 'test':
+        ETHEC_trainer.test()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", help='Use DEBUG mode.', action='store_true')
+    parser.add_argument("--lr", help='Input learning rate.', type=float, default=0.01)
+    parser.add_argument("--batch_size", help='Batch size.', type=int, default=8)
+    parser.add_argument("--evaluator", help='Evaluator type.', type=str, default='ML')
+    parser.add_argument("--experiment_name", help='Experiment name.', type=str, required=True)
+    parser.add_argument("--experiment_dir", help='Experiment directory.', type=str, required=True)
+    parser.add_argument("--image_dir", help='Image parent directory.', type=str, required=True)
+    parser.add_argument("--n_epochs", help='Number of epochs to run training for.', type=int, required=True)
+    parser.add_argument("--n_workers", help='Number of workers.', type=int, default=4)
+    parser.add_argument("--eval_interval", help='Evaluate model every N intervals.', type=int, default=1)
+    parser.add_argument("--resume", help='Continue training from last checkpoint.', action='store_true')
+    parser.add_argument("--model", help='NN model to use.', type=str, required=True)
+    parser.add_argument("--freeze_weights", help='This flag fine tunes only the last layer.', action='store_true')
+    parser.add_argument("--set_mode", help='If use training or testing mode (loads best model).', type=str,
+                        required=True)
+    args = parser.parse_args()
+
+    ETHEC_train_model(args)

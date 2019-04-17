a71a71877c8e58bd7fd7a56147e46db7f80b37be
ankitdhall <ankitdhall2005@yahoo.co.in>
2019-04-17 16:43:08 +0200

    add cifar10 for profiling

--- network/profiling.py
+++ network/profiling.py
@@ -1,7 +1,9 @@
 from network.ethec_experiments import ETHEC_train_model
+from network.finetuner import train_cifar10
 import argparse
 
-if __name__ == '__main__':
+
+def ethec_trainer():
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", help='Use DEBUG mode.', action='store_true')
     parser.add_argument("--lr", help='Input learning rate.', type=float, default=0.01)
@@ -28,3 +30,32 @@ if __name__ == '__main__':
                               '--resume'])
 
     ETHEC_train_model(args)
+
+
+def cifar_trainer():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", help='Use DEBUG mode.', action='store_true')
+    parser.add_argument("--lr", help='Input learning rate.', type=float, default=0.01)
+    parser.add_argument("--batch_size", help='Batch size.', type=int, default=8)
+    parser.add_argument("--evaluator",
+                        help='Evaluator type. If using `multi_level` option for --loss then is overidden.', type=str,
+                        default='ML')
+    parser.add_argument("--experiment_name", help='Experiment name.', type=str, required=True)
+    parser.add_argument("--experiment_dir", help='Experiment directory.', type=str, required=True)
+    parser.add_argument("--n_epochs", help='Number of epochs to run training for.', type=int, required=True)
+    parser.add_argument("--n_workers", help='Number of workers.', type=int, default=4)
+    parser.add_argument("--eval_interval", help='Evaluate model every N intervals.', type=int, default=1)
+    parser.add_argument("--resume", help='Continue training from last checkpoint.', action='store_true')
+    parser.add_argument("--model", help='NN model to use.', type=str, required=True)
+    parser.add_argument("--loss", help='Loss function to use.', type=str, required=True)
+    parser.add_argument("--freeze_weights", help='This flag fine tunes only the last layer.', action='store_true')
+    parser.add_argument("--set_mode", help='If use training or testing mode (loads best model).', type=str,
+                        required=True)
+    cmd = """--n_epochs 10 --experiment_name cifar_hierarchical_ft_debug --experiment_dir ../exp --debug --evaluator MLST --model alexnet --loss multi_level --set_mode train"""
+    args = parser.parse_args(cmd.split(' '))
+
+    train_cifar10(args)
+
+
+if __name__ == '__main__':
+    cifar_trainer()

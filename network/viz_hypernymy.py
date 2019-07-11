41d8e252325a93641d100a687ec640bd087d96ad
ankitdhall <ankitdhall2005@yahoo.co.in>
2019-07-11 17:37:55 +0200

    viz label embeddings

--- network/viz_hypernymy.py
+++ network/viz_hypernymy.py
@@ -41,15 +41,16 @@ import matplotlib
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 
-from network.order_embeddings import OrderEmbedding, OrderEmbeddingLoss, SimpleEuclideanEmbLoss, Embedder, EmbeddingMetrics, ETHECHierarchy
-from network.order_embeddings_images import create_imageless_dataloaders
+from network.oe import Embedder
+from network.oe import create_imageless_dataloaders
 from network.inference import Inference
 
 
 class VizualizeGraphRepresentation:
     def __init__(self, debug=False,
                  dim=2,
-                 weights_to_load='/home/ankit/learning_embeddings/exp/ethec_debug/oe_debug/oe_rm_50/weights/best_model.pth'):
+                 weights_to_load='/home/ankit/learning_embeddings/exp/ethec_debug/ec/bs10/oe2d_a_1.0_lr_0.1_1.0_ppl/weights/best_model.pth'):
+                 # weights_to_load='/home/ankit/Desktop/hypernym_viz/oe_2d/best_model.pth'):
         torch.manual_seed(0)
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -58,21 +59,32 @@ class VizualizeGraphRepresentation:
         if debug:
             labelmap = ETHECLabelMapMergedSmall()
 
-        dataloaders = create_imageless_dataloaders(debug=debug)
+        # dataloaders = create_imageless_dataloaders(debug=debug)
 
-        G = nx.DiGraph()
-        for index, data_item in enumerate(dataloaders['train']):
-            inputs, labels, level_labels = data_item['image'], data_item['labels'], data_item['level_labels']
-            for level_id in range(len(labelmap.levels)-1):
-                for sample_id in range(level_labels.shape[0]):
-                    G.add_edge(level_labels[sample_id, level_id].item()+labelmap.level_start[level_id],
-                               level_labels[sample_id, level_id+1].item()+labelmap.level_start[level_id+1])
+        # G = nx.DiGraph()
+        # for index, data_item in enumerate(dataloaders['train']):
+        #     inputs, labels, level_labels = data_item['image'], data_item['labels'], data_item['level_labels']
+        #     for level_id in range(len(labelmap.levels)-1):
+        #         for sample_id in range(level_labels.shape[0]):
+        #             G.add_edge(level_labels[sample_id, level_id].item()+labelmap.level_start[level_id],
+        #                        level_labels[sample_id, level_id+1].item()+labelmap.level_start[level_id+1])
+
+        if debug:
+            path_to_folder = '../database/ETHEC/ETHECSmall_embeddings/graphs'
+        else:
+            path_to_folder = '../database/ETHEC/ETHEC_embeddings/graphs'
+
+        G = nx.read_gpickle(os.path.join(path_to_folder, 'G'))
 
         self.G = G
         self.G_tc = nx.transitive_closure(self.G)
         self.labelmap = labelmap
 
-        self.model = Embedder(embedding_dim=dim, labelmap=labelmap, normalize=False)
+        if 'ec' in weights_to_load:
+            self.model = Embedder(embedding_dim=dim, labelmap=labelmap, normalize=False, K=3.0)
+        else:
+            self.model = Embedder(embedding_dim=dim, labelmap=labelmap, normalize=False)#, K=3.0)
+        self.model =nn.DataParallel(self.model)
 
         self.load_model(weights_to_load)
 
@@ -94,11 +106,13 @@ class VizualizeGraphRepresentation:
         self.model.eval()
 
         colors = ['c', 'm', 'y', 'k']
-        embeddings_x, embeddings_y, annotation, color_list = [], [], [], []
+        embeddings_x, embeddings_y, annotation, color_list = {}, {}, {}, {}
 
         connected_to = {}
 
-        for level_id in range(len(self.labelmap.levels)):
+        fig, ax = plt.subplots()
+
+        for level_id in range(3): #len(self.labelmap.levels)):
             level_start, level_stop = self.labelmap.level_start[level_id], self.labelmap.level_stop[level_id]
             level_color = colors[level_id]
             for label_ix in range(self.labelmap.levels[level_id]):
@@ -106,22 +120,26 @@ class VizualizeGraphRepresentation:
                 emb = self.model(torch.tensor([emb_id]).to(self.device)).cpu().detach()
                 emb = emb[0].numpy()
 
-                embeddings_x.append(emb[0])
-                embeddings_y.append(emb[1])
-                annotation.append('{}'.format(getattr(self.labelmap,
+                embeddings_x[emb_id] = emb[0]
+                embeddings_y[emb_id] = emb[1]
+                annotation[emb_id] = '{}'.format(getattr(self.labelmap,
                                                       '{}_ix_to_str'.format(self.labelmap.level_names[level_id]))[label_ix]
-                                                 ))
-                color_list.append(level_color)
+                                                 )
+                color_list[emb_id] = level_color
 
                 connected_to[emb_id] = [v for u, v in list(self.G.edges(emb_id))]
 
-        fig, ax = plt.subplots()
-        ax.scatter(embeddings_x, embeddings_y, c=color_list)
+                ax.scatter(emb[0], emb[1], c=level_color, alpha=1)
+                # ax.annotate(annotation[emb_id], (emb[0], emb[1]))
+
+        # fig, ax = plt.subplots()
+        # ax.scatter(embeddings_x, embeddings_y, c=color_list)
 
         for from_node in connected_to:
             for to_node in connected_to[from_node]:
-                plt.plot([embeddings_x[from_node], embeddings_x[to_node]], [embeddings_y[from_node], embeddings_y[to_node]],
-                         'b-')
+                if to_node in embeddings_x:
+                    plt.plot([embeddings_x[from_node], embeddings_x[to_node]], [embeddings_y[from_node], embeddings_y[to_node]],
+                             'b-', alpha=0.2)
 
         # for i, txt in enumerate(annotation):
         #     ax.annotate(txt, (embeddings_x[i], embeddings_y[i]))

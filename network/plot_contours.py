1e3331b132f652be57404c5fb055e1709a03668c
ankitdhall <ankitdhall2005@yahoo.co.in>
2019-07-16 20:21:20 +0200

    add voronoi

--- network/plot_contours.py
+++ network/plot_contours.py
@@ -6,7 +6,9 @@ import matplotlib
 # matplotlib.use('pdf')
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
-import markdown
+
+from scipy.spatial import Voronoi, voronoi_plot_2d
+
 
 def func(X, Y, vec, thresh=0.0):
     X = X/np.sqrt(X**2+Y**2)
@@ -15,6 +17,7 @@ def func(X, Y, vec, thresh=0.0):
     dot_p = X*vec[0] + Y*vec[1]
     dot_p[dot_p < thresh] = -1
     return dot_p
+
 
 def get_summary(path_to_summary):
     f = open(path_to_summary, 'r')
@@ -27,6 +30,7 @@ def get_summary(path_to_summary):
 
 
 def plot_2d_emb(save_to_disk=True):
+    # path_to_emb = '/home/ankit/Desktop/emb_weights/cnn2d/embedding_info.npy'
     path_to_emb = '/home/ankit/Desktop/emb_weights/cnn2d/embedding_info.npy'
     summary = get_summary(os.path.join(os.path.dirname(path_to_emb), 'summary.md'))
     emb_info = np.load(path_to_emb).item()
@@ -45,18 +49,20 @@ def plot_2d_emb(save_to_disk=True):
 
     for label_ix in embeddings_x:
         # ax.scatter(embeddings_x[label_ix], embeddings_y[label_ix], c=color_list[label_ix], alpha=max(0.05, summary[annotation[label_ix]][1]))
-        ax.scatter(embeddings_x[label_ix], embeddings_y[label_ix], c=color_list[label_ix], alpha=max(0.0, summary[annotation[label_ix]][0]/max_val[color_list[label_ix]]))
+        # ax.scatter(embeddings_x[label_ix], embeddings_y[label_ix], c=color_list[label_ix], alpha=max(0.0, summary[annotation[label_ix]][0]/max_val[color_list[label_ix]]))
+        ax.scatter(embeddings_x[label_ix], embeddings_y[label_ix], c=color_list[label_ix], alpha=1)
 
-        if color_list[label_ix] not in ['y', 'k']:
+        # if color_list[label_ix] not in ['y', 'k']:
             # ax.annotate(annotation[label_ix], (embeddings_x[label_ix], embeddings_y[label_ix]))
             # ax.annotate(str(summary[annotation[label_ix]][0]), (embeddings_x[label_ix], embeddings_y[label_ix]))
-            ax.annotate('{:.4f}, {}'.format(summary[annotation[label_ix]][1], summary[annotation[label_ix]][0]), (embeddings_x[label_ix], embeddings_y[label_ix]))
+            # ax.annotate('{:.4f}, {}'.format(summary[annotation[label_ix]][1], summary[annotation[label_ix]][0]), (embeddings_x[label_ix], embeddings_y[label_ix]))
 
-    # for from_node in connected_to:
-    #     for to_node in connected_to[from_node]:
-    #         if to_node in embeddings_x:
-    #             plt.plot([embeddings_x[from_node], embeddings_x[to_node]], [embeddings_y[from_node], embeddings_y[to_node]],
-    #                      'b-', alpha=0.2)
+
+    for from_node in connected_to:
+        for to_node in connected_to[from_node]:
+            if to_node in embeddings_x:
+                plt.plot([embeddings_x[from_node], embeddings_x[to_node]], [embeddings_y[from_node], embeddings_y[to_node]],
+                         'b-', alpha=0.2)
 
     # plot contour for
     # delta = 0.01
@@ -70,8 +76,19 @@ def plot_2d_emb(save_to_disk=True):
     # ax.scatter(embeddings_x[plot_contours_for_label_id], embeddings_y[plot_contours_for_label_id], c='r', alpha=1)
     # plt.plot([0.0, embeddings_x[plot_contours_for_label_id]], [0.0, embeddings_y[plot_contours_for_label_id]], 'r-',
     #          alpha=1.0)
-
     ax.axis('equal')
+
+    points = []
+    for label_ix in embeddings_x:
+        if color_list[label_ix] == 'm':
+            points.append([embeddings_x[label_ix], embeddings_y[label_ix]])
+    points = np.array(points)
+
+    # compute Voronoi tesselation
+    vor = Voronoi(points)
+    fig = voronoi_plot_2d(vor, ax, show_vertices=False, line_colors='orange', line_width=2)
+
+
     if save_to_disk:
         fig.set_size_inches(8, 7)
         fig.savefig(os.path.join(os.path.dirname(path_to_emb), 'embedding.pdf'), dpi=200)

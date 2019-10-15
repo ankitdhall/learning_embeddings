18fec218f865f14a6382d2aa2bdb5ea8be79419a
ankitdhall <ankitdhall2005@yahoo.co.in>
2019-10-15 10:03:34 +0200

    add manual seed

--- network/ethec_experiments.py
+++ network/ethec_experiments.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from __future__ import division
 import torch
+torch.manual_seed(0)
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
@@ -17,6 +18,7 @@ from network.loss import MultiLevelCELoss, MultiLabelSMLoss, LastLevelCELoss, Ma
 
 from PIL import Image
 import numpy as np
+np.random.seed(0)
 
 import copy
 import argparse
